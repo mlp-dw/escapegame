7b8bd3906e8cadd205b59456f1a52f54208ce05a
mlp-dw <95212024+mlp-dw@users.noreply.github.com>
2024-04-29 08:54:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let x = 0;
 let layers = document.querySelectorAll("img.images"); 
 let key = document.querySelector(".key"); 
+let valid_1 = document.querySelector(".France"); 
+let valid_2 = document.querySelector(".US"); 
+let valid_3 = document.querySelector(".Canada"); 
 
 function destinationSlider(){  
    
@@ -16,14 +19,31 @@ function destinationSlider(){
 let myInterval = setInterval(destinationSlider, 1000)
 
 function checkAnswer(number){
-  if(number === 1 || number === 5 || number ===10){
-    stop()
+   if (number === 1 || number === 5 || number === 10) {
     for (i = number; i < layers.length; i++) {
-    layers[i].style.display = 'none';
-   }
-    key.style.display = "block"
+      layers[i].style.display = 'none';
+    }
     
-    setTimeout(()=>{location.reload()},4000)
+    switch (number) {
+      case 1:
+        valid_1.style.display = "block";
+        break;
+      case 5:
+        valid_2.style.display = "block";
+        break;
+      case 10:
+        valid_3.style.display = "block";
+        break;
+    }
+    
+    let isValid1 = valid_1.style.display === "block";
+    let isValid2 = valid_2.style.display === "block";
+    let isValid3 = valid_3.style.display === "block";
+    
+    if (isValid1 && isValid2 && isValid3) {
+      stop();
+      key.style.display = "block";
+    }
   }
 }
 
